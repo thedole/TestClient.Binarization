f55494c1c73b763b6ee130bc3bdd8d75c8d30778
Dag Ole Kristoffersen <thedole@gmail.com>
2016-04-05 23:23:05 +0200

    fixed namespace on pathToBitmapConverter, added it as a resource and used it for the source image binding

--- ValueConverters/PathToBitmapImageConverter.cs
+++ ValueConverters/PathToBitmapImageConverter.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
-namespace dok.wpf.exploration.ValueConverters
+namespace TestClient.Binarization.ValueConverters
 {
     public class PathToBitmapImageConverter : IValueConverter
     {
